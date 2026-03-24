654a742f4369add1e6fe2b79f1fe960882d0e62b
Ggg <ksyushashmel10.10@gmail.com>
2026-03-24 19:49:23 +0700

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,14 @@
+let lastScroll = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+    
+    if (currentScroll > lastScroll && currentScroll > 100) {
+        header.classList.add('hidden');
+    } else {
+        header.classList.remove('hidden');
+    }
+    
+    lastScroll = currentScroll;
+});
